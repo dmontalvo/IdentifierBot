be8737ec2e8ce9b8850b7b876cba2f64546d1639
Daniel <daniel.m@archive.org>
2010-06-23 17:14:43 +0000

    final version

--- idadder.py
+++ idadder.py
@@ -1,4 +1,4 @@
-csvfile = 'ltids.csv'
+csvfile = 'LibraryThing_to_OpenLibrary.csv'
 db = 'ids.sqlite'
 
 import csv
@@ -7,8 +7,13 @@ import sqlite3
 import _init_path
 from openlibrary.api import OpenLibrary
 
-ol = OpenLibrary('http://0.0.0.0:8080')
-ol.autologin()
+ol = OpenLibrary('http://openlibrary.org/')
+for attempt in range(5):
+    try:
+        ol.autologin()
+        break
+    except:
+        print 'ol.autologin error, retrying'
 conn = sqlite3.connect(db)
 c = conn.cursor()
 reader = csv.reader(open(csvfile), delimiter='\t', quotechar='|')
@@ -20,7 +25,13 @@ for row in reader:
     x = c.fetchone()
     if x != None:
         continue
-    data = ol.get(key)
+    print 'Trying to get key: %r' % key
+    for attempt in range(5):
+        try:
+            data = ol.get(key)
+            break
+        except:
+            print 'ol.get() error, retrying'
     if data.has_key('identifiers'):
         if data['identifiers'].has_key('librarything'):
             if data['identifiers']['librarything'].count(ltid) == 0:
@@ -29,6 +40,11 @@ for row in reader:
             data['identifiers']['librarything'] = [ltid]
     else:
         data['identifiers'] = {'librarything': [ltid]}
-    ol.save(key, data, 'Testing IdentifierBot')  
+    for attempt in range(5):
+        try:
+            print ol.save(key, data, 'added LibraryThing ID')
+            break
+        except:
+            print 'ol.save() error; retrying'
     c.execute('insert into ids values (?, ?)', (key, ltid))
     conn.commit()
